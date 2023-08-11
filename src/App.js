9c8969cb876e2d2f914da1537e0aa6cc02d521b1
mooky1007 <mooky1007@naver.com>
2023-08-11 10:29:05 +0900

    tesing for realtime database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { getDatabase, ref, get, child } from 'firebase/database';
+import { useEffect, useState } from 'react';
+
 const App = () => {
-  return (
-    <div>
-      Firebase
-    </div>
-  );
-}
+    const db = getDatabase();
+    const [datas, setDatas] = useState([]);
+
+    useEffect(() => {
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, '/posts'))
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    setDatas(Object.values(snapshot.val()));
+                } else {
+                    console.log('No data available');
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+    return (
+        <>
+            <div>Firebase</div>
+            <ul>
+                {datas.map((el, idx) => <li key={idx}>{el}</li>)}
+            </ul>
+        </>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBYGGjuU8XgznvNyvputV11HizaF7KzNHc",
+  authDomain: "my-first-firebase-7761e.firebaseapp.com",
+  databaseURL: "https://my-first-firebase-7761e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "my-first-firebase-7761e",
+  storageBucket: "my-first-firebase-7761e.appspot.com",
+  messagingSenderId: "368086354740",
+  appId: "1:368086354740:web:154fab71849c4d8edb7361"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
